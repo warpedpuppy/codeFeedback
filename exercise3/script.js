bdabea50b8f71fcb433a69c718c4aacbd803a14c
ted <ted@warpedpuppy.com>
2022-03-15 13:48:02 -0400

    changed variable name to FOO

--- exercise3/script.js
+++ exercise3/script.js
@@ -2,7 +2,7 @@
 //instructions: in the space indicated, write code that will make sure the variable FuctionOne is of datatype object.  After you write your code click "run" to see if you have the correct answer.
 
  
-const FunctionOne = (function () {
+const FOO = (function () {
 	// write your code below this line
   
   
@@ -13,14 +13,14 @@ const FunctionOne = (function () {
   
   // DO NOT LOOK BELOW THIS LINE
 
-  if (!FunctionOne) {
-	console.log('FunctionOne is undefined!')
-  } else if (Array.isArray(FunctionOne)){
-	console.log('FunctionOne has a value, but it is not an object, FunctionOne is an array.')
-  } else if (typeof FunctionOne !== 'object'){
-	console.log('FunctionOne has a value, but it is not an object, FunctionOne is a', typeof FunctionOne, '.')
-  }else if (typeof FunctionOne === 'object') {
-	console.log('Yes!  Go you!  That is an FunctionOne is of datatype object!')
+  if (!FOO) {
+	console.log('FOO is undefined!')
+  } else if (Array.isArray(FOO)){
+	console.log('FOO has a value, but it is not an object, FOO is an array.')
+  } else if (typeof FOO !== 'object'){
+	console.log('FOO has a value, but it is not an object, FOO is a', typeof FOO, '.')
+  }else if (typeof FOO === 'object') {
+	console.log('Yes!  Go you!  That is an FOO is of datatype object!')
   }
   
   
